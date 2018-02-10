55692607512a26cbfbdaf31ba1b467730b0703a2
pbeppe <palmisano.giuseppe@live.com>
2018-02-10 14:25:52 +0100

    Soluzione Finale

--- app/src/main/java/it/appacademy/gokuevegeta/MainActivity.java
+++ app/src/main/java/it/appacademy/gokuevegeta/MainActivity.java
@@ -7,57 +7,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    android.app.FragmentManager fm = getFragmentManager();
-    FragmentTransaction tx = fm.beginTransaction();
+public class MainActivity extends AppCompatActivity
+{
 
     // GOKUEVEGETA
     // porca miseria non cambia
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction tx = fm.beginTransaction();
+        Fragment fr;
 
         // TODO Aggiungere il FragmentUno
-      /*  FragmentUno fragmentUno = new FragmentUno();
-        tx.add(R.id.fragment_place, fragmentUno);
-        tx.commit();*/
+        fr = new FragmentUno();
+        tx.add(R.id.fragment_place, fr);
+        tx.commit();
 
     }
 
-    public void clickVegeta(View view) {
-        FragmentDue fragmentDue = new FragmentDue();
-       if (tx.isEmpty())
-       {
-
-         tx.add(R.id.fragment_place, fragmentDue);
-         tx.commit();
-       }
-        {
-            tx.replace(R.id.fragment_place, fragmentDue);
-            tx.commit();
-        }
-
-
-
+    public void clickVegeta(View view)
+    {
+        Fragment fr;
+        fr = new FragmentDue();
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction tx = fm.beginTransaction();
+        tx.replace(R.id.fragment_place, fr);
+        tx.commit();
     }
 
     public void clickGoku(View view)
-    {
-        FragmentUno fragmentUno = new FragmentUno();
-        if (tx.isEmpty()){
+    {   Fragment fr;
+        fr = new FragmentUno();
+        android.app.FragmentManager fm = getFragmentManager();
+        FragmentTransaction tx = fm.beginTransaction();
+        tx.replace(R.id.fragment_place, fr);
+        tx.commit();}
 
-            tx.add(R.id.fragment_place, fragmentUno);
-            tx.commit();}
-            else
-        {
-            tx.add(R.id.fragment_place, fragmentUno);
-            tx.commit();}
-        }
 
     }
 
