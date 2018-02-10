b2f3dcb8461384f09494a2c3d57c6afe2e21ef23
pbeppe <palmisano.giuseppe@live.com>
2018-02-10 12:25:11 +0100

    Introduzione Vegeta

--- app/src/main/java/it/appacademy/gokuevegeta/MainActivity.java
+++ app/src/main/java/it/appacademy/gokuevegeta/MainActivity.java
@@ -5,9 +5,11 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    android.app.FragmentManager fm = getFragmentManager();
+    FragmentTransaction tx = fm.beginTransaction();
 
     // GOKUEVEGETA
     // porca miseria non cambia
@@ -16,15 +18,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.app.FragmentManager fm = getFragmentManager();
 
 
-        FragmentTransaction tx = fm.beginTransaction();
+
 
         // TODO Aggiungere il FragmentUno
-        FragmentUno fragmentUno = new FragmentUno();
+      /*  FragmentUno fragmentUno = new FragmentUno();
         tx.add(R.id.fragment_place, fragmentUno);
-        tx.commit();
+        tx.commit();*/
 
     }
-}
+
+    public void clickVegeta(View view) {
+        FragmentDue fragmentDue = new FragmentDue();
+       if (tx.isEmpty())
+       {
+
+         tx.add(R.id.fragment_place, fragmentDue);
+         tx.commit();
+       }
+        {
+            tx.replace(R.id.fragment_place, fragmentDue);
+            tx.commit();
+        }
+
+
+
+    }
+
+    public void clickGoku(View view)
+    {
+        FragmentUno fragmentUno = new FragmentUno();
+        if (tx.isEmpty()){
+
+            tx.add(R.id.fragment_place, fragmentUno);
+            tx.commit();}
+            else
+        {
+            tx.add(R.id.fragment_place, fragmentUno);
+            tx.commit();}
+        }
+
+    }
+
